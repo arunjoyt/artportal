c18aa7839dc4d60a74dcb82e7ca214deef07eff6
arunjoyt <16219238+arunjoyt@users.noreply.github.com>
2025-05-27 17:03:36 +0530

    web page

--- artportal/www/art.py
+++ artportal/www/art.py
@@ -1,16 +1,20 @@
 import frappe
 from frappe.utils.file_manager import save_file
-from frappe.utils import now
+import base64
 
 @frappe.whitelist(allow_guest=True)
-def upload_image(image_data=None, filename=None, artist=None):
-    
-    if not image_data or not artist:
-        frappe.throw("Image and Artist are required")
+def get_images():
+    # Allow everyone
+    return frappe.get_all("Artwork", fields=["name", "image", "artist"], order_by="creation desc")
 
-    # Decode base64 image and save
-    from frappe.utils.file_manager import save_file
-    import base64
+
+@frappe.whitelist()
+def upload_image(image_data=None, filename=None, artist=None):
+    if frappe.session.user == "Guest":
+        frappe.throw("Login required to upload images.")
+
+    if not image_data or not artist:
+        frappe.throw("Missing image or artist")
 
     content = base64.b64decode(image_data)
     file_doc = save_file(filename, content, dt=None, dn=None, folder="Home", is_private=0)
@@ -20,14 +24,14 @@ def upload_image(image_data=None, filename=None, artist=None):
         "image": file_doc.file_url,
         "artist": artist
     })
-    doc.insert(ignore_permissions=True)
+    doc.insert()
     return doc
 
-@frappe.whitelist(allow_guest=True)
-def get_images():
-    return frappe.get_all("Artwork", fields=["name", "image", "artist"], order_by="creation desc")
 
-@frappe.whitelist(allow_guest=True)
+@frappe.whitelist()
 def delete_image(name):
-    frappe.delete_doc("Artwork", name, ignore_permissions=True)
+    if frappe.session.user == "Guest":
+        frappe.throw("Login required to delete images.")
+
+    frappe.delete_doc("Artwork", name)
     return "Deleted"
